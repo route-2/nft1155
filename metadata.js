c572cac9a84fc41e3e5d8323c1ad5a7a7fb2d453
route-2 <ruthurao@gmail.com>
2022-07-22 16:20:32 +0530

    update

---
+++ metadata.js
@@ -0,0 +1,18 @@
+const fs=require("fs");
+const args = process.argv.slice(2);
+const inputFolder = args[0];
+const dir = `${__dirname}\\${inputFolder}`;
+const inputFiles = fs.readdirSync(dir).sort()
+
+
+
+  console.log(inputFiles)
+
+
+  inputFiles.forEach(file){
+      const file = inputFiles[i]
+      fs.renameSync(`${dir}\\${file}`,`${dir}\\${i+1}.${file.split('.')[1]}`) 
+  }
+
+ 
+ 
